f972950b31d1fe9cef3128dd9b2a60fcf956600c
Luke Merrett <lukeamerrett@gmail.com>
2017-02-20 23:07:33 +0000

    Removed all code, now a blank slate

--- Exercise/TurtleRunner.fs
+++ Exercise/TurtleRunner.fs
@@ -3,38 +3,23 @@ module TurtleRunner
 open System
 
 // Basic discriminted unions - think enums
-type PenState = Up | Down
-type Colour = Black | Red | Blue | Green
-type TurnDirection = Left | Right
+type PenState = FillInThePenStates
+type Colour = FillInTheColours
+type TurnDirection = FillInTheTurnDirections
 
 // Record type - translates to a sealed class with readonly, immutable properties
 type Turtle = {
-    xpos:float
-    ypos:float
-    angle:float
-    penState:PenState
-    colour:Colour
+    turtleProperty:string
 }
 
 // Discriminated unions with values - think enums on steroids
-type Command = 
-    | Move of float
-    | Turn of TurnDirection * float 
-    | SetPen of PenState 
-    | SetColour of Colour
+type Command = FillInTheAvailableCommands
 
 // Function to apply state changes in a Command to a Turtle
+// Uses pattern matching to deconstruct the command into the different cases
 let processCommand turtle command = 
     match command with
-    | Move distance -> let angleInRads = turtle.angle * (Math.PI/180.0) * 1.0
-                       {turtle with 
-                            xpos = turtle.xpos + (distance * Math.Cos(angleInRads))
-                            ypos = turtle.ypos + (distance * Math.Sin(angleInRads))}
-    | Turn(direction, degrees) -> match direction with 
-                                  | Left ->  {turtle with angle = turtle.angle + degrees} 
-                                  | Right -> {turtle with angle = turtle.angle - degrees} 
-    | SetPen state ->  {turtle with penState=state}
-    | SetColour colour -> {turtle with colour=colour}
+    | _ -> failwith "Command pattern matching not applied"
 
 // --- Uncomment this section to run a full test ---
 
