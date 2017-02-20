4daaee52949299fcdb3aa7bfa58e1000d67ffbd3
Luke Merrett <lukeamerrett@gmail.com>
2017-02-20 21:25:10 +0000

    Added tests against the union types

--- Unit-Tests/Tests.fs
+++ Unit-Tests/Tests.fs
@@ -3,7 +3,32 @@
 open System
 open Xunit
 open TurtleRunner
+open Microsoft.FSharp.Reflection
 
-[<Fact>]
-let ``Given a `` = 
-    ()
+let private getUnionTypeFromString<'a> (s:string) =
+    match FSharpType.GetUnionCases typeof<'a> |> Array.filter (fun case -> case.Name = s) with
+    |[|case|] -> Some(FSharpValue.MakeUnion(case,[||]) :?> 'a)
+    |_ -> None
+
+[<Theory>]
+[<InlineData("Up")>]
+[<InlineData("Down")>]
+let ``Expected PenState types are defined`` expectedType = 
+    let item = getUnionTypeFromString<PenState>(expectedType)
+    Assert.True(item.IsSome)
+
+[<Theory>]
+[<InlineData("Black")>]
+[<InlineData("Red")>]
+[<InlineData("Blue")>]
+[<InlineData("Green")>]
+let ``Expected Colour types are defined`` expectedType = 
+    let item = getUnionTypeFromString<Colour>(expectedType)
+    Assert.True(item.IsSome)
+
+[<Theory>]
+[<InlineData("Left")>]
+[<InlineData("Right")>]
+let ``Expected TurnDirection types are defined`` expectedType = 
+    let item = getUnionTypeFromString<TurnDirection>(expectedType)
+    Assert.True(item.IsSome)
